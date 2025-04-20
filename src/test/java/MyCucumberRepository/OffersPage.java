a8eca757b79149afa949c5d82615d71ccae4b826
pallavilambate <160910405+pallavilambate@users.noreply.github.com>
2025-04-20 20:30:10 +0530

    Update OffersPage.java

---
+++ src/test/java/MyCucumberRepository/OffersPage.java
@@ -0,0 +1,11 @@
+package MyCucumberRepository;
+
+public class OffersPage {
+  WebDriver driver;
+    OffersPage(WebDriver driver){
+      this.driver=driver;
+}
+public void searchproduct(){
+  
+}
+}
